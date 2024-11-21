215a59e236301bb60a9337879115b4d120c6220c
Arsen <arsen.test.mess@gmail.com>
2024-11-21 11:51:01 +0200

    Added endpoints to urls.py: genres, actors, theatre_halls, plays, performances and reservations. Used routers from DRF

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,5 +1,23 @@
 from django.urls import path, include
+from rest_framework import routers
 
-urlpatterns = []
+from theatre.views import (
+    GenreViewSet,
+    ActorViewSet,
+    TheatreHallViewSet,
+    PlayViewSet,
+    PerformanceViewSet,
+    ReservationViewSet,
+)
+
+router = routers.DefaultRouter()
+router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+router.register("theatre_halls", TheatreHallViewSet)
+router.register("plays", PlayViewSet)
+router.register("performances", PerformanceViewSet)
+router.register("reservations", ReservationViewSet)
+
+urlpatterns = [path("", include(router.urls))]
 
 app_name = "theatre"
