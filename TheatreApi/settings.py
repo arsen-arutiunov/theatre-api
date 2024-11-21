c444df585c49fc58ede38870499efcce3a289bbd
Arsen <arsen.test.mess@gmail.com>
2024-11-21 10:52:10 +0200

    Added AUTH_USER_MODEL to project settings

--- TheatreApi/settings.py
+++ TheatreApi/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "user",
+    "theatre"
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "user.User"
